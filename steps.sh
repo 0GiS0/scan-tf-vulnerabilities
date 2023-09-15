c800284134bf02b4fe1ae9f3db8d35b7550b7152
Gisela <giselatb@outlook.com>
2023-09-15 12:48:00 +0200

    Update steps.sh

--- steps.sh
+++ steps.sh
@@ -1,35 +1,25 @@
-# Scan using tfsec https://github.com/aquasecurity/tfsec 
+# Scan using trivy https://github.com/aquasecurity/trivy
 # ------------------------------------------------------------------------------
-# tfsec -h
-# tfsec .
 
-# Tfsec > Trivy
-# ------------------------------------------------------------------------------
 trivy config . 
 trivy config  --format table --exit-code  0 --severity  UNKNOWN,LOW,MEDIUM,HIGH,CRITICAL  .
 
-# Scan using checkov
+# Scan using checkov https://github.com/bridgecrewio/checkov
 # ------------------------------------------------------------------------------
 checkov -h
 # Scan only terraform files
 checkov -d . --output cli --output sarif --output-file-path console,results.sarif --soft-fail    
 
-# Scan using terrascan
+# Scan using terrascan https://github.com/tenable/terrascan
 # ------------------------------------------------------------------------------
 terrascan -h
 terrascan scan -d .
 
-# Scan using snyk
+# Scan using snyk https://github.com/snyk/cli
 # ------------------------------------------------------------------------------
 snyk -h
 snyk auth
 snyk iac test --help
-SNYK_TOKEN=1a0bee11-920a-477a-9c9f-3dcf40b26a41 snyk iac test . --sarif
-
-# Test a Terraform plan file
-terraform init \
-
-
-terraform plan -out=tfplan.binary
-terraform show -json tfplan.binary > tf-plan.json
-snyk iac test tf-plan.json
+SNYK_TOKEN=1a0bee11-920a-477a-9c9f-3dcf40b26a41
+snyk config set api=$SNYK_TOKEN
+snyk iac test . --sarif
